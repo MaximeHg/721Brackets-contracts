a50575663623182e9b7b46762d7302ed67ad0738
Maxime Hagenbourger <maximehg@gmail.com>
2019-03-09 16:50:40 +0100

    :white_check_mark: added a test for minting from core

--- test/BracketTest.js
+++ test/BracketTest.js
@@ -28,4 +28,20 @@ contract('BracketCore', function(accounts) {
 
   });
 
+  describe('Minting from Core', () => {
+    let BracketCoreInstance;
+
+    before(async () => {
+      BracketCoreInstance = await BracketCore.new();
+    });
+
+    it('Should be able to mint a token id #0 with no predictions', async () => {
+      let predictionsArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+      await BracketCoreInstance.mint(accounts[0], predictionsArray);
+      let owner = await BracketCoreInstance.ownerOf(0);
+      assert.equal(owner, accounts[0]);
+    });
+
+  });
+
 });
